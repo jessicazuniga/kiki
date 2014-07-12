8d9c35700e8ceee62e3877c8e6e7e983bdfca836
Jessica Zuniga <jessicazuniga@gmail.com>
2014-07-12 11:39:24 -0700

    add functionality to select events

--- kiki/app/controllers/home_controller.rb
+++ kiki/app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	@current_datetime = Time.new
+	@current_weekday = @current_datetime.wday
+  	@week_events = Event.getNextEvents(@current_datetime, 70)
   end
 end
--- kiki/app/models/event.rb
+++ kiki/app/models/event.rb
@@ -2,4 +2,17 @@ class Event < ActiveRecord::Base
 	has_many :attendances
 	has_many :comments
 	has_many :related_events
+
+def self.getNextEvents(time, num_days)
+	start_time = time.beginning_of_day
+	end_time = (time + num_days.days).end_of_day
+
+	start_time_string = start_time.to_s(:db)
+	end_time_string = end_time.to_s(:db)
+
+	# Event.find_by_sql("""select * from events where time <= '#{end_time_string}' order by time;""")
+	Event.find_by_sql("""select * from events where time >= '#{start_time_string}' and time <= '#{end_time_string}' order by time;""")
 end
+
+end
+
