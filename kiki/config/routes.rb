f6aa7e018df0172bde762a3e3fb15da3364d404d
Jessica Zuniga <jessicazuniga@gmail.com>
2014-07-13 17:15:35 -0700

    create event controller view

--- kiki/config/routes.rb
+++ kiki/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
 root 'home#index'
+get 'event' => 'event#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
