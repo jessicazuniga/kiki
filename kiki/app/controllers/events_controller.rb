5f18e357cecf6c9176a0ffa7a4be636708148ae6
Gabor Foldes <gfoldes@lendingclub.com>
2014-07-20 10:27:04 -0700

    fix tab view and jqm ajax calls

--- kiki/app/controllers/events_controller.rb
+++ kiki/app/controllers/events_controller.rb
@@ -4,6 +4,11 @@ class EventsController < ApplicationController
   	@current_datetime = Time.new
 	@current_weekday = @current_datetime.wday
   	@week_events = Event.getNextEvents(@current_datetime, 70)
+
+	respond_to do |format|
+        format.html { render :layout => !request.xhr? }
+    end
+
   end
 
   def show
@@ -11,6 +16,11 @@ class EventsController < ApplicationController
   	@event = Event.find(event_id)
   	@participants = Attendance.getEventParticipants(event_id)
   	@comments = Comment.getEventComments(event_id)
+
+	respond_to do |format|
+        format.html { render :layout => !request.xhr? }
+    end
+
   end
 
 end
