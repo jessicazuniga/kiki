b1639650568311df78fc10ed5e22031864b7cc4c
Gabor Foldes <gabor.foldes@gmail.com>
2014-09-16 01:32:19 -0700

    adding registration

--- kiki/app/controllers/application_controller.rb
+++ kiki/app/controllers/application_controller.rb
@@ -27,4 +27,12 @@ class ApplicationController < ActionController::Base
     session[:previous_url] || root_path
   end
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
 end
