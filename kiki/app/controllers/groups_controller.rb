a7eb7f266c1c7d43f8cef8924b76e5f87bef5c25
Gabor Foldes <gabor.foldes@gmail.com>
2014-10-15 22:17:20 -0700

    getting rid of more funky routing

--- kiki/app/controllers/groups_controller.rb
+++ kiki/app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 
   def index
-  	redirect_to events_path if cookies.has_key?(:current_group_id) && !cookies[:current_group_id].nil?
+  	#redirect_to events_path if cookies.has_key?(:current_group_id) && !cookies[:current_group_id].nil?
     @groups = Group.order(:name)
   end
 
