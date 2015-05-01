32d21a186adde07d6157a804c8f9ff25b3acd7d9
Gabor Foldes <gabor.foldes@gmail.com>
2015-04-30 23:09:51 -0700

    fixed utf8 encoding error in event description

--- kiki/config/application.rb
+++ kiki/config/application.rb
@@ -20,5 +20,7 @@ module Kiki
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.encoding = 'utf-8'
+
   end
 end
